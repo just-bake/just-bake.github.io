6e79e6a749e13fa7f9b7050b60b3fea25ebe7f33
just-bake <justbakediva99@gmail.com>
2025-03-05 17:55:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ let config = {
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
-    BLOOM_INTENSITY: 0.8,
+    BLOOM_INTENSITY: 0.4,
     BLOOM_THRESHOLD: 0.6,
     BLOOM_SOFT_KNEE: 0.7,
     SUNRAYS: true,
